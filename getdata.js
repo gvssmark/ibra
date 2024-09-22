2fff93779176de946d27a76201614294c93bcf87
gvssmark <83894487+gvssmark@users.noreply.github.com>
2024-09-22 15:25:50 +0530

    Update getdata.js

--- getdata.js
+++ getdata.js
@@ -10,3 +10,4 @@ url='https://script.google.com/macros/s/AKfycbwIVAlRrCTt6z_Ej4W6PHhopFqCq1JvEsSo
 ({id,name,add1,add2,add3,district,place,pincode,fixed,mobile1,mobile2,state,email,srno,sbac,doj:ddmmyy(doj),dor:ddmmyy(dor),spouse,area,blank,dob:ddmmyy(dob),fampen,pensionbr,scanned}))
 activeMembers =data.filter(a => a.name !== "").sort(function (a, b) { return a.name.localeCompare(b.name) });
 }
+</script>
