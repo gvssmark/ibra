4bde16f140be53a04f53982ba760ccb678fd15ae
gvssmark <83894487+gvssmark@users.noreply.github.com>
2025-09-08 07:44:07 +0530

    Update sevaparameters.js

--- sevaparameters.js
+++ sevaparameters.js
@@ -1,9 +1,10 @@
 
-startdate =''  //seva start date
-enddate   =''  //seva end date
-year=   //seva year
-lastDateForPhase1='' //Phase1 end date
-lastDateForPhase2='' //Phase2 end date
+startdate ='2027-09-15'  //seva start date
+enddate   ='2027-09-30'  //seva end date
+year= 2027  //seva year
+lastDateForPhase1=2027-09-10'' //Phase1 end date
+lastDateForPhase2='2027-09-25' //Phase2 end date
+
 
 
 
