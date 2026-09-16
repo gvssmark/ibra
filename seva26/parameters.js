d53f0c8dc4cc02ce953eef255e59d416efc51035
gvssmark <83894487+gvssmark@users.noreply.github.com>
2026-09-16 18:06:34 +0530

    Update parameters.js

--- seva26/parameters.js
+++ seva26/parameters.js
@@ -89,6 +89,8 @@ const PARAMETERS = {
     "Mother",
     "Paternal Grand Mother",
     "Maternal Grand Mother",
+    "Father in Law",
+    "Mother in Law",
     "Elder Brother",
     "Elder Brother's Wife",
     "Younger Brother",
